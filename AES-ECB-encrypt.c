e13388474ebf818ef62609453ce69ef761429ad5
Aditi Gupta <aditigupta@Aditis-MacBook-Pro.local>
2019-05-21 16:53:17 -0400

    clean 2

--- AES-ECB-encrypt.c
+++ AES-ECB-encrypt.c
@@ -57,57 +57,9 @@ int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
     return ciphertext_len;
 }
 
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-            unsigned char *iv, unsigned char *plaintext)
-{
-    EVP_CIPHER_CTX *ctx;
-
-    int len;
-
-    int plaintext_len;
-
-    /* Create and initialise the context */
-    if(!(ctx = EVP_CIPHER_CTX_new()))
-        handleErrors();
-
-    /*
-     * Initialise the decryption operation. IMPORTANT - ensure you use a key
-     * and IV size appropriate for your cipher
-     * In this example we are using 256 bit AES (i.e. a 256 bit key). The
-     * IV size for *most* modes is the same as the block size. For AES this
-     * is 128 bits
-     */
-    if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_ecb(), NULL, key, iv))
-        handleErrors();
-
-    /*
-     * Provide the message to be decrypted, and obtain the plaintext output.
-     * EVP_DecryptUpdate can be called multiple times if necessary.
-     */
-    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, ciphertext, ciphertext_len))
-        handleErrors();
-    plaintext_len = len;
-
-    /*
-     * Finalise the decryption. Further plaintext bytes may be written at
-     * this stage.
-     */
-    if(1 != EVP_DecryptFinal_ex(ctx, plaintext + len, &len))
-        handleErrors();
-    plaintext_len += len;
-
-    /* Clean up */
-    EVP_CIPHER_CTX_free(ctx);
-
-    return plaintext_len;
-}
-
 int main (void)
 {
-    /*
-     * Set up the key and iv. Do I need to say to not hard code these in a
-     * real application? :-)
-     */
+    /* Set up the key and iv */
 
 		 /* A 256 bit key */
 		 unsigned char key[256];
@@ -124,7 +76,7 @@ int main (void)
 		char* to_append_decoded;
 		size_t test;
 		Base64Decode(to_append, &to_append_decoded, &test);
-		
+
 		/* Message to be encrypted */
 		char *text = "The quick brown fox jumps over the lazy dog";
 		char p[strlen(text) + strlen(to_append_decoded)];
@@ -139,10 +91,7 @@ int main (void)
      */
     unsigned char ciphertext[128];
 
-    /* Buffer for the decrypted text */
-    unsigned char decryptedtext[128];
-
-    int decryptedtext_len, ciphertext_len;
+    int ciphertext_len;
 
     /* Encrypt the plaintext */
     ciphertext_len = encrypt (plaintext, strlen ((char *)plaintext), key, iv,
