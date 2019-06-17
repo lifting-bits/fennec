d4f8fcb1d9d58a22ebc6b530c4e9caa8e7ae150b
Aditi Gupta <aditi100gupta@gmail.com>
2019-06-17 14:34:54 -0400

    call original function from replacement version

---
+++ llvm_patching/replaceIVstatic.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+unsigned char *generate_iv_original() {
+  unsigned char *result = (unsigned char *)"";
+  return result;
+}
+
+unsigned char *replacement() {
+  unsigned char *iv = generate_iv_original();
+  return iv;
+}
