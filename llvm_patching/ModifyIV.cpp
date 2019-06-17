edb0e33f62f46f35b37f57117da2bdcd7d543556
Aditi Gupta <aditi100gupta@gmail.com>
2019-06-17 13:40:30 -0400

    more generalizable

--- llvm_patching/ModifyIV.cpp
+++ llvm_patching/ModifyIV.cpp
@@ -13,34 +13,40 @@ namespace {
     ModifyIV() : FunctionPass(ID) {}
 
     bool runOnFunction(Function &F) override {
-      if (F.getName() == "generate_iv") {
+      // if (F.getName() == "generate_iv") {
       // Get the function to call from our runtime library.
         LLVMContext &Ctx = F.getContext();
         Type *retType = Type::getInt8PtrTy(Ctx);
         FunctionType *logFuncType = FunctionType::get(retType, false);
-        FunctionCallee logFunc = F.getParent()->getOrInsertFunction("replacement", logFuncType);
+        FunctionCallee newFunction =
+                F.getParent()->getOrInsertFunction("replacement", logFuncType);
 
         for (auto &B : F) {
           for (auto &I : B) {
-            if (auto *op = dyn_cast<ReturnInst>(&I)) {
-              IRBuilder<> builder(op);
-              // Insert *before `op`.
-              builder.SetInsertPoint(&I);
+            if (auto *op = dyn_cast<CallInst>(&I)) {
+              if (op->getCalledFunction()->getName() == "generate_iv") {
+                IRBuilder<> builder(op);
+                // Insert *before `op`.
+                // builder.SetInsertPoint(&I);
 
-              // Insert a call to our function.
-              Value* result = builder.CreateCall(logFunc);
-              I.setOperand(I.getNumOperands() - 1, result);
-              return true;
+                // Insert a call to our function.
+                Value* newCall = builder.CreateCall(newFunction);
+                for (auto& U : op->uses()) {
+                  User* user = U.getUser();
+                  user->setOperand(U.getOperandNo(), newCall);
+                }
+                // I.setOperand(I.getNumOperands() - 1, result);
+                // return true;
+              }
             }
           }
+          return true;
         }
-      }
 
       return false;
     }
   };
 }
-	
+
 char ModifyIV::ID = 0;
 static RegisterPass<ModifyIV> X("modifyIV", "ModifyIV Pass", false, false);
-
