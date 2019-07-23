39bd14027f52d3e9c512daf7e43a25a113dc9a13
Aditi Gupta <aditi100gupta@gmail.com>
2019-07-23 11:15:40 -0400

    fix typo

--- llvm-setup.sh
+++ llvm-setup.sh
@@ -6,12 +6,10 @@ mv llvm-4.0.1.src llvm
 cd llvm
 mkdir build
 cd build
-make
 cmake -G "Unix Makefiles" ..
+make
 mv ../../ReplaceFunction ../lib/Transforms
 echo "add_subdirectory(ReplaceFunction)" >> ../lib/Transforms/CMakeLists.txt
 make
 version=$(./bin/opt --version | grep "LLVM version" | grep -o [0-9\.]* | xargs)
 python -c "assert($version=='4.0.1')"
-
-# python -c "assert()" some version parse stuff
