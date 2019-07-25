29ed59f311ebc38cffdfc184ee5f94d839e812c1
Aditi Gupta <aditi100gupta@gmail.com>
2019-07-25 17:24:24 -0400

    stripped tests

--- test.sh
+++ test.sh
@@ -27,6 +27,28 @@ else
   status=1
 fi
 
+# test arguments mode 1
+bash run.sh 1 $remillPath $idaPath tests/arguments/mode1/withArgs tests/arguments/mode1/withArgsReplacement generate replacement
+argumentsMode1=$(./tests/arguments/mode1/withArgs.new | xargs)
+if [ $argumentsMode1 = '4' ]
+then
+  echo Arguments Mode 1 passed
+else
+  echo Arguments Mode 1 failed
+  status=1
+fi
+
+# test arguments mode 2
+bash run.sh 2 $remillPath $idaPath tests/arguments/mode2/withArgs tests/arguments/mode2/withArgsReplacement generate replacement generate_original
+argumentsMode2=$(./tests/arguments/mode2/withArgs.new | xargs)
+if [ $argumentsMode2 = '4' ]
+then
+  echo Arguments Mode 2 passed
+else
+  echo Arguments Mode 2 failed
+  status=1
+fi
+
 # test encryption mode 1
 bash run.sh 1 $remillPath $idaPath tests/crypto/mode1/encrypt tests/crypto/mode1/replaceIV_mode1 generate_iv replacement -lcrypto
 encryptMode1Test1=$(./tests/crypto/mode1/encrypt.new "" | head -n 1 | xargs)
@@ -51,25 +73,27 @@ else
   status=1
 fi
 
-# test arguments mode 1
-bash run.sh 1 $remillPath $idaPath tests/arguments/mode1/withArgs tests/arguments/mode1/withArgsReplacement generate replacement
-argumentsMode1=$(./tests/arguments/mode1/withArgs.new | xargs)
-if [ $argumentsMode1 = '4' ]
+# test stripped encryption mode 1
+bash run.sh 1 $remillPath $idaPath tests/stripped_crypto/mode1/encryptStripped tests/stripped_crypto/mode1/replaceIV_mode1 sub_400df0 replacement -lcrypto
+strippedMode1Test1=$(./tests/stripped_crypto/mode1/encryptStripped.new "" | head -n 1 | xargs)
+strippedMode1Test2=$(./tests/stripped_crypto/mode1/encryptStripped.new "" | head -n 1 | xargs)
+if [ $strippedMode1Test1 != $strippedMode1Test2 ]
 then
-  echo Arguments Mode 1 passed
+  echo Stripped Crypto Mode 1 passed
 else
-  echo Arguments Mode 1 failed
+  echo Stripped Crypto Mode 1 failed
   status=1
 fi
 
-# test arguments mode 2
-bash run.sh 2 $remillPath $idaPath tests/arguments/mode2/withArgs tests/arguments/mode2/withArgsReplacement generate replacement generate_original
-argumentsMode2=$(./tests/arguments/mode2/withArgs.new | xargs)
-if [ $argumentsMode2 = '4' ]
+# test stripped encryption mode 2
+bash run.sh 2 $remillPath $idaPath tests/stripped_crypto/mode2/encryptStripped tests/stripped_crypto/mode2/replaceIV_mode2 sub_400df0 replacement generate_iv_original -lcrypto
+strippedMode2Test1=$(./tests/stripped_crypto/mode2/encryptStripped.new "" | head -n 1 | xargs)
+strippedMode2Test2=$(./tests/stripped_crypto/mode2/encryptStripped.new "" | head -n 1 | xargs)
+if [ $strippedMode2Test1 != $strippedMode2Test2 ]
 then
-  echo Arguments Mode 2 passed
+  echo Stripped Crypto Mode 2 passed
 else
-  echo Arguments Mode 2 failed
+  echo Stripped Crypto Mode 2 failed
   status=1
 fi
 
